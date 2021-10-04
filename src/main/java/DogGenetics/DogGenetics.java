adac424e3c4fbe5408b7e4f5668b09e16e545f7b
yatu360 <yatu360@gmail.com>
2021-10-05 00:20:05 +0100

    011: Dog's Genetics complete

---
+++ src/main/java/DogGenetics/DogGenetics.java
@@ -0,0 +1,36 @@
+package DogGenetics;
+
+import java.util.*;
+
+public class DogGenetics {
+
+
+    public static void main(String[] args) {
+
+        String [] breeds = {"German Shepard", "Dachshund", "Shiba Inu", "Great Dane", "Dalmation"};
+        int [] percentage = new int[5];
+        Random rand1 = new Random();
+        int c = 100;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your dog's name? ");
+        String dogName = input.nextLine();
+        System.out.println("Well then, I have this highly reliable report on "+dogName+"'s prestigious background right here.");
+
+        for (int i = 0; i < percentage.length-1; i++){
+            if (c>0) {
+                percentage[i] = rand1.nextInt(c) + 1;
+                c -= percentage[i];
+            }else{
+                percentage[i] = 0;
+            }
+        }
+        percentage[percentage.length-1] = c;
+
+        System.out.println(dogName + " "+ "is:");
+        for (int j = 0; j<breeds.length; j++){
+            System.out.printf("%d%% %s %n", percentage[j], breeds[j]);
+        }
+
+    }
+}

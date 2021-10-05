3bd0cd2e3ff0c8044bfedb3544ee69bbaa85ed95
yatu360 <yatu360@gmail.com>
2021-10-05 10:32:51 +0100

    012: HeartyHearts completed

---
+++ src/main/java/HeartyHearts/HeartyHearts.java
@@ -0,0 +1,22 @@
+package HeartyHearts;
+
+import java.util.*;
+
+public class HeartyHearts {
+
+    public static void healthCheck(int age){
+        int maxHeartRate = 220 - age;
+        int lower = (int)Math.round(maxHeartRate*0.5);
+        int upper = (int)Math.round(maxHeartRate*0.85);
+        System.out.println("Your maximum heart rate should be "+maxHeartRate+" beats per minute");
+        System.out.println("Your target HR Zone is "+lower+" - "+upper+" beats per minute");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        healthCheck(age);
+
+
+    }
+}
